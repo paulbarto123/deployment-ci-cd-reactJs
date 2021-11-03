41d635d8146013830ea71ac10b41282190ab417c
Paulus Siahaan <siahaan.paulus123@gmail.com>
2021-11-03 10:41:53 +0700

    update dashboardprojectpage.js

--- src/pages/member/DashboardProjectPage/index.jsx
+++ src/pages/member/DashboardProjectPage/index.jsx
@@ -1,11 +1,70 @@
 import React from "react";
-import { Divider, Tabs } from "antd";
+import { Divider, Tabs, Col, Row, Card, Space, Spin } from "antd";
+import Text from "antd/lib/typography/Text";
 
+import useGetArticles from "../../../Query/useGetArticles";
 import SidebarMemberComponent from "../../../components/SidebarMemberComponent";
-import ArticleList from "./ArticleList.js";
+
 import ProjectList from "./ProjectList.js";
 
 const { TabPane } = Tabs;
+const ArticleComponent = (props) => {
+  return (
+    <>
+      <Card
+        title=""
+        bordered={true}
+        style={{ width: 1120, backgroundColor: "AliceBlue" }}
+      >
+        <>
+          <Row>
+            <Col span={5}>
+              <h4>
+                <b>Kategori</b>
+              </h4>
+              <p>Judul</p>
+              <p>Tanggal Post</p>
+              <p>Ditulis Oleh User ID</p>
+              <p>Isi Artikel</p>
+            </Col>
+            <Col span={19}>
+              <h4>
+                : <b>{props.article.kategori}</b>
+              </h4>
+              <p>: {props.article.judul}</p>
+              <p>: {props.article.posting_date}</p>
+              <p>: {props.article.id_user}</p>
+              <p>
+                :
+                {/* <ReadMore> */}
+                  {props.article.isi_artikel}
+                {/* </ReadMore> */}
+              </p>
+            </Col>
+          </Row>
+        </>
+      </Card>
+    </>
+  );
+};
+const ArticleList = () => {
+  const { data, isLoading } = useGetArticles();
+  return (
+    <div className="card">
+      <Space direction="vertical">
+        {isLoading ? (
+          <Spin tip="Loading..."></Spin>
+        ) : data ? (
+          data.results.map((article) => (
+            <ArticleComponent key={article.id_artikel} article={article} />
+          ))
+        ) : (
+          <Text>Gagal memuat data</Text>
+        )}
+      </Space>
+    </div>
+  );
+};
 
 const DashboardProjectPage = () => {
   return (
