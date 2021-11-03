b2a854b9bfdecb16cf3d7ac3c1d1891ed6d1b363
Paulus Siahaan <siahaan.paulus123@gmail.com>
2021-11-03 10:45:27 +0700

    update dashboardprojectpage.js

--- src/pages/member/DashboardProjectPage/index.jsx
+++ src/pages/member/DashboardProjectPage/index.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import { Divider, Tabs, Col, Row, Card, Space, Spin } from "antd";
 import Text from "antd/lib/typography/Text";
-
+import moment from "moment";
 import useGetArticles from "../../../Query/useGetArticles";
 import SidebarMemberComponent from "../../../components/SidebarMemberComponent";
-
-import ProjectList from "./ProjectList.js";
+import useGetProjectList from "../../../Query/useGetProjectList";
 
 const { TabPane } = Tabs;
 const ArticleComponent = (props) => {
@@ -62,6 +61,65 @@ const ArticleList = () => {
           <Text>Gagal memuat data</Text>
         )}
       </Space>
+    </div>
+  );
+};
+
+
+const MyProjectComponent = (props) => {
+  //console.log("props data >>", props.data);
+  return (
+    <Card
+      title=""
+      bordered={true}
+      style={{ width: 1120, backgroundColor: "AliceBlue" }}
+    >
+      <>
+        <Row key={props.data.id_project}>
+          <Col span={5}>
+            <h4>
+              <b>Kategori Project</b>
+            </h4>
+            <p>Nama Project</p>
+            <p>Tanggal Mulai</p>
+            <p>Link Trello</p>
+            <p>Deskripsi Project</p>
+            <p>Creator ID</p>
+          </Col>
+          <Col span={19}>
+            <h4>
+              : <b>{props.data.kategori_project}</b>
+            </h4>
+            <p>: {props.data.nama_project}</p>
+            <p>: {moment(props.data.start_date).format("DD MMMM YYYY")}</p>
+            <p>: {props.data.link_trello}</p>
+            <p>: {props.data.deskripsi_project}</p>
+            <p>
+              :
+              {" "}<span>{props.data.creator} </span>
+
+            </p>
+          </Col>
+        </Row>
+        <Divider />
+      </>
+    </Card>
+  );
+};
+const ProjectList = () => {
+  const { data, isLoading } = useGetProjectList();
+  return (
+    <div className="card">
+      {isLoading ? (
+        <Spin tip="Loading..."></Spin>
+      ) : data ? (
+        data.results.map((project) => (
+          // console.log("project value >> ", project)
+          <MyProjectComponent key={project.id_project} data={project} />
+        ))
+      ) : (
+        <Text>Gagal memuat data</Text>
+      )}
     </div>
   );
 };
